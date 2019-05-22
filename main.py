171dafe3cbaf91b6639bc33f472eb6bb26151461
Paul <pkutrich@gmail.com>
2019-05-21 17:06:54 -0700

    Finish cleaning script.

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import re
+import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', 500)
 
 
-columns = ['YEAR', 'NUMADULT', 'HHADULT', 'SEX', '_AGEG5YR', '_PRACE1', 'CHILDREN', '_CHLDCNT', 'EMPLOY', 'EMPLOY1',
-           'INCOME2', '_BMI5CAT', 'GENHLTH', 'PREDIAB1', 'DIABETE3', '_PAINDEX', '_PAINDX1', 'TSDROVE']
+columns = ['YEAR', 'NUMADULT', 'HHADULT', '_AGEG5YR', '_PRACE1', 'CHILDREN', '_CHLDCNT', 'EMPLOY', 'EMPLOY1',
+           'INCOME2', '_BMI5CAT', 'GENHLTH', 'PREDIAB1', 'DIABETE3', '_PAINDEX', '_PAINDX1']
 
-drop_from_both = [column.lower() for column in ['NUMADULT', 'HHADULT', '_CHLDCNT', 'EMPLOY', 'EMPLOY1', '_BMI5CAT', 'GENHLTH',
-                  'PREDIAB1', 'DIABETE3', '_PAINDEX', '_PAINDX1']]
+drop_from_both = [column.lower() for column in ['NUMADULT', 'HHADULT', '_CHLDCNT', 'EMPLOY', 'EMPLOY1', '_BMI5CAT',
+                                                'GENHLTH', 'PREDIAB1', 'DIABETE3', '_PAINDEX', '_PAINDX1']]
 
 columns_2017 = ['FRUITJU2', 'FRUIT2', 'FVGREEN1', 'VEGETAB2', 'FRENCHF1', 'POTATOE1', 'FTJUDA2_', 'FRUTDA2_',
                 'GRENDA1_', 'VEGEDA2_', 'FRNCHDA_', 'POTADA1_', '_MISFRT1', '_MISVEG1', '_FRTRES1',  '_VEGRES1',
@@ -26,86 +27,139 @@ columns_2017 = [column.lower() for column in columns_2017]
 all_columns = columns + columns_2017 + drop_from_2017
 
 
-df_all_years = pd.read_stata("C:\\Users\Paul\PycharmProjects\BlogPost\data\WA_BRFSS_11to17_B.dta",
-                             columns=all_columns, convert_missing=False)
+df = pd.read_stata("C:\\Users\Paul\PycharmProjects\BlogPost\data\WA_BRFSS_11to17_B.dta",
+                   columns=all_columns, convert_missing=False)
 
-df_all_years['numadult'] = df_all_years['numadult'].map(lambda x: 1 if x in [77, 99] else x)
-df_all_years['hhadult'] = df_all_years['hhadult'].map(lambda x: 1 if x in [77, 99] else x)
+df['numadult'] = df['numadult'].map(lambda x: 1 if x in [77, 99] else x)
+df['hhadult'] = df['hhadult'].map(lambda x: 1 if x in [77, 99] else x)
 
-df_all_years['numadult'] = df_all_years['numadult'].fillna(1)
-df_all_years['hhadult'] = df_all_years['hhadult'].fillna(1)
+df['numadult'] = df['numadult'].fillna(1)
+df['hhadult'] = df['hhadult'].fillna(1)
 
-df_all_years['adults'] = np.maximum(df_all_years['hhadult'], df_all_years['numadult'])
-df_all_years['adults'] = df_all_years['adults'].map(lambda x: 1 if x == 0 else x)
+df['adults'] = np.maximum(df['hhadult'], df['numadult'])
+df['adults'] = df['adults'].map(lambda x: 1 if x == 0 else x)
 
-df_all_years['sex'] = df_all_years['sex'].map({9: 2, 'Male': 0, 'Female': 1})
+df['children'] = df['children'].map(lambda x: 0 if x in [88, 99] else x)
 
-df_all_years['children'] = df_all_years['children'].map(lambda x: 0 if x in [88, 99] else x)
+df['total_household'] = df['adults'] + df['children']
 
-df_all_years['total_household'] = df_all_years['adults'] + df_all_years['children']
-
-df_all_years['unemployed'] = df_all_years['employ'].str.contains('Unemployed') | df_all_years['employ1'].str.contains('unemployed')
+df['unemployed'] = df['employ'].str.contains('Unemployed') | df['employ1'].str.contains('unemployed')
 
 bmi_dict = {'Over': True, 'Obese': True, 'Normal': False, 'Under': False}
-df_all_years['overweight'] = df_all_years['_bmi5cat'].map(bmi_dict)
+df['overweight'] = df['_bmi5cat'].map(bmi_dict)
 
-df_all_years['zips'] = df_all_years['zipcode'] + df_all_years['zipcode1']
-df_all_years['zips'] = df_all_years['zips'].str.strip()
-df_all_years['zips'] = [int(x) if x != '' else np.nan for x in df_all_years['zips']]
+df['zips'] = df['zipcode'] + df['zipcode1']
+df['zips'] = df['zips'].str.strip()
+df['zips'] = [int(x) if x != '' else np.nan for x in df['zips']]
+
+
+def non_seattle_zip(x):
+    if not re.match(r'98\d\d\d[.]0', str(x)):
+        return 99999
+    else:
+        return x
+
+
+df['zips'] = df['zips'].map(non_seattle_zip)
+df = df[(df['zips'] != 99999) & (df['zips'] != 77777)]
+
+
+desert_zips_repeats = [98126,
+                       98106,
+                       98168,
+                       98106,
+                       98108,
+                       98146,
+                       98166,
+                       98146,
+                       98188,
+                       98138,
+                       98168,
+                       98178,
+                       98057,
+                       98178,
+                       98057,
+                       98057,
+                       98058,
+                       98055,
+                       98055,
+                       98058,
+                       98058,
+                       98188,
+                       98198,
+                       98188,
+                       98198,
+                       98032,
+                       98198,
+                       98002,
+                       98001,
+                       98047,
+                       98047,
+                       98001,
+                       98022]
+
+desert_zips = list(set(desert_zips_repeats))
+
+df['desert'] = df['zips'].map(lambda x: True if x in desert_zips else False)
 
 health_dict = {'Very Good': True, 'Good': True, 'Excellent': True, 'Fair': False, 'Poor': False, 'DK': False, 'Missing': True}
-df_all_years['good-health'] = df_all_years['genhlth'].map(health_dict)
+df['good-health'] = df['genhlth'].map(health_dict)
 
-df_all_years['pre-diab'] = df_all_years['prediab1'].map(lambda x: True if x == 'Yes' else False)
+df['prediab1'] = df['prediab1'].fillna('DK')
+df['pre-diab'] = df['prediab1'].map(lambda x: True if x == 'Yes' else False)
 
-df_all_years['diabetic'] = df_all_years['diabete3'].map(lambda x: True if x in ['Yes', 'Borderline/Pre-Diabetes'] else False)
+df['diabetic'] = df['diabete3'].map(lambda x: True if x in ['Yes', 'Borderline/Pre-Diabetes'] else False)
 
-df_all_years['_paindex'] = df_all_years['_paindex'].map(lambda x: True if x == 'Meets Aerobic Recs' else False)
-df_all_years['_paindx1'] = df_all_years['_paindx1'].map(lambda x: True if x == 'Meets Aerobic Recs' else False)
+df['_paindex'] = df['_paindex'].map(lambda x: True if x == 'Meets Aerobic Recs' else False)
+df['_paindx1'] = df['_paindx1'].map(lambda x: True if x == 'Meets Aerobic Recs' else False)
 
-df_all_years['active'] = ((df_all_years['_paindex'] is True) | df_all_years['_paindx1'] is True)
+df['active'] = ((df['_paindex'] is True) | df['_paindx1'] is True)
 
-df_all_years['vegeda1_'] = df_all_years['vegeda1_'].fillna(0)
+df['vegeda1_'] = df['vegeda1_'].fillna(0)
 
-df_all_years['vegeda2_'] = df_all_years['vegeda2_'].fillna(0)
+df['vegeda2_'] = df['vegeda2_'].fillna(0)
 
-df_all_years['frutda1_'] = df_all_years['frutda1_'].fillna(0)
+df['frutda1_'] = df['frutda1_'].fillna(0)
 
-df_all_years['frutda2_'] = df_all_years['frutda2_'].fillna(0)
+df['frutda2_'] = df['frutda2_'].fillna(0)
 
-df_all_years['ftjuda1_'] = df_all_years['ftjuda1_'].fillna(0)
+df['ftjuda1_'] = df['ftjuda1_'].fillna(0)
 
-df_all_years['ftjuda2_'] = df_all_years['ftjuda2_'].fillna(0)
+df['ftjuda2_'] = df['ftjuda2_'].fillna(0)
 
-df_all_years['_vegesum'] = df_all_years['_vegesum'].fillna(0)
+df['_vegesum'] = df['_vegesum'].fillna(0)
 
-df_all_years['_vegesu1'] = df_all_years['_vegesu1'].fillna(0)
+df['_vegesu1'] = df['_vegesu1'].fillna(0)
 
-df_all_years['_frutsum'] = df_all_years['_frutsum'].fillna(0)
+df['_frutsum'] = df['_frutsum'].fillna(0)
 
-df_all_years['_frutsu1'] = df_all_years['_frutsu1'].fillna(0)
+df['_frutsu1'] = df['_frutsu1'].fillna(0)
 
-df_all_years['veg_day'] = df_all_years['vegeda1_'] + df_all_years['vegeda2_']
+df['veg_day'] = df['vegeda1_'] + df['vegeda2_']
 
-df_all_years['fruit_day'] = df_all_years['frutda1_'] + df_all_years['frutda2_']
+df['fruit_day'] = df['frutda1_'] + df['frutda2_']
 
-df_all_years['juice_day'] = df_all_years['ftjuda1_'] + df_all_years['ftjuda2_']
+df['juice_day'] = df['ftjuda1_'] + df['ftjuda2_']
 
-df_all_years['veg_sum'] = df_all_years['_vegesum'] + df_all_years['_vegesu1']
+df['veg_sum'] = df['_vegesum'] + df['_vegesu1']
 
-df_all_years['fruit_sum'] = df_all_years['_frutsum'] + df_all_years['_frutsu1']
+df['fruit_sum'] = df['_frutsum'] + df['_frutsu1']
 
-df_all_years['_frtlt1'] = df_all_years['_frtlt1'].map({'One or more per day': 1, 'Less than once per day': 0, 'Missing': 0})
+df['_frtlt1'] = df['_frtlt1'].map({'One or more per day': 1, 'Less than once per day': 0, 'Missing': 0})
 
-df_all_years['_frtlt1a'] = df_all_years['_frtlt1a'].map({'One or more per day': 1, 'Less than once per day': 0, 'Missing': 0})
+df['_frtlt1a'] = df['_frtlt1a'].map({'One or more per day': 1, 'Less than once per day': 0, 'Missing': 0})
 
-df_all_years['_veglt1'] = df_all_years['_veglt1'].map({'One or more per day': 1, 'Less than once per day': 0, 'Missing': 0})
+df['_veglt1'] = df['_veglt1'].map({'One or more per day': 1, 'Less than once per day': 0, 'Missing': 0})
 
-df_all_years['_veglt1a'] = df_all_years['_veglt1a'].map({'One or more per day': 1, 'Less than once per day': 0, 'Missing': 0})
-# TODO:  fix the everyday from above
-df_all_years['fruit_everyday'] = (df_all_years['fruit_day'] > 0) & (df_all_years['fruit_sum'] < 16)
+df['_veglt1a'] = df['_veglt1a'].map({'One or more per day': 1, 'Less than once per day': 0, 'Missing': 0})
 
-df_all_years['veg_everyday'] = (df_all_years['veg_day'] > 0) & (df_all_years['veg_sum'] < 23)
+df['fruit_everyday'] = (df['_frtlt1'] == 1) | (df['_frtlt1a'] == 1)
+
+df['fruit_everyday'] = (df['fruit_day'] > 0) & (df['fruit_sum'] < 16)
+
+df['veg_everyday'] = (df['_veglt1'] == 1) | (df['_veglt1a'] == 1)
+
+df['veg_everyday'] = (df['veg_day'] > 0) & (df['veg_sum'] < 23)
 
 
 def per_week(x):
@@ -136,25 +190,48 @@ def per_month_17(x):
         return False
 
 
-df_all_years['veg_every_week'] = (df_all_years['vegetab1'].map(per_week) | df_all_years['vegetab2'].map(per_week_17))
+df['veg_every_week'] = (df['vegetab1'].map(per_week) | df['vegetab2'].map(per_week_17))
 
-df_all_years['fruit_every_week'] = (df_all_years['fruit1'].map(per_week) | df_all_years['fruit2'].map(per_week_17))
+df['fruit_every_week'] = (df['fruit1'].map(per_week) | df['fruit2'].map(per_week_17))
 
-df_all_years['veg_every_month'] = (df_all_years['vegetab1'].map(per_month) | df_all_years['vegetab2'].map(per_month_17))
+df['veg_every_month'] = (df['vegetab1'].map(per_month) | df['vegetab2'].map(per_month_17))
 
-df_all_years['fruit_every_month'] = (df_all_years['fruit1'].map(per_month) | df_all_years['fruit2'].map(per_month_17))
+df['fruit_every_month'] = (df['fruit1'].map(per_month) | df['fruit2'].map(per_month_17))
 
-df11 = df_all_years[df_all_years['year'] == 2011]
+final_colunns = ['year', '_ageg5yr', '_prace1', 'income2', 'adults', 'children', 'total_household', 'zips', 'desert',
+                 'unemployed', 'active', 'overweight', 'pre-diab', 'diabetic', 'good-health', 'fruit_everyday',
+                 'veg_everyday', 'fruit_every_week', 'veg_every_week', 'fruit_every_month', 'veg_every_month']
 
-df13 = df_all_years[df_all_years['year'] == 2013]
+df = df[final_colunns]
 
-df15 = df_all_years[df_all_years['year'] == 2015]
+final_names = ['Year', 'Age', 'Race', 'Income', 'Adults', 'Children', 'Total_household', 'Zip-code', 'Food-Desert',
+               'Unemployed', 'Active', 'Overweight', 'Pre-Diabetic', 'Diabetic', 'Good-health', 'Fruit_everyday',
+               'Veg_everyday', 'Fruit_every_week', 'Veg_every_week', 'Fruit_every_month', 'Veg_every_month']
 
-df17 = df_all_years[df_all_years['year'] == 2017]
+df.columns = final_names
 
-df11 = df11.drop(columns_2017 + drop_from_both + drop_from_2017, axis=1)
-df13 = df13.drop(columns_2017 + drop_from_both + drop_from_2017, axis=1)
-df15 = df15.drop(columns_2017 + drop_from_both + drop_from_2017, axis=1)
-df17 = df17.drop(drop_from_2017 + drop_from_both + columns_2017, axis=1)
+df11 = df[df['Year'] == 2011]
 
-dataframes = [df11, df13, df15, df17]
+df13 = df[df['Year'] == 2013]
+
+df15 = df[df['Year'] == 2015]
+
+df17 = df[df['Year'] == 2017]
+
+# df11 = df11.drop(columns_2017 + drop_from_both + drop_from_2017, axis=1)
+# df13 = df13.drop(columns_2017 + drop_from_both + drop_from_2017, axis=1)
+# df15 = df15.drop(columns_2017 + drop_from_both + drop_from_2017, axis=1)
+# df17 = df17.drop(drop_from_2017 + drop_from_both + columns_2017, axis=1)
+
+
+df11_oasis = df11[df11['Food-Desert'] == 0]
+df13_oasis = df13[df13['Food-Desert'] == 0]
+df15_oasis = df15[df15['Food-Desert'] == 0]
+df17_oasis = df17[df17['Food-Desert'] == 0]
+
+df11_desert = df11[df11['Food-Desert'] == 1]
+df13_desert = df13[df13['Food-Desert'] == 1]
+df15_desert = df15[df15['Food-Desert'] == 1]
+df17_desert = df17[df17['Food-Desert'] == 1]
+
+print(df15_desert.head())
